34e2d399bd4bcc050431cbc6fdc2096a077c8331
Moazam05 <salmanmoazam08@gmail.com>
2024-10-16 13:15:40 +0500

    Scaling

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+import LoginImageDark from './login_dark_animation.png';
+import LoginImageLight from './login_animation_light.png';
+
+export const Images = {
+  LoginImageDark,
+  LoginImageLight,
+};
--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -1,13 +1,60 @@
-import {Text} from 'react-native';
+import {Image, StyleSheet, useColorScheme, View} from 'react-native';
 import React from 'react';
 import CustomSafeAreaView from '../../components/CustomSafeAreaView';
+import CustomText from '../../components/CustomText';
+import {FONTS} from '../../constants/Fonts';
+import {Images} from '../../assets/images';
+import {
+  normalizeModerately,
+  screenHeight,
+  screenWidth,
+} from '../../utils/Scaling';
 
 const LoginScreen = () => {
+  const theme = useColorScheme();
   return (
     <CustomSafeAreaView>
-      <Text>LoginScreen</Text>
+      <View style={styles.container}>
+        <CustomText variant="h3" fontFamily={FONTS.Medium}>
+          Together we Grow
+        </CustomText>
+        <CustomText variant="h7" style={styles.subText} fontFamily={FONTS.Bold}>
+          Invest • Pay • Loans
+        </CustomText>
+
+        <View style={styles.imgContainer}>
+          <Image
+            style={styles.img}
+            source={
+              theme === 'dark' ? Images.LoginImageDark : Images.LoginImageLight
+            }
+          />
+        </View>
+      </View>
     </CustomSafeAreaView>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  subText: {
+    marginTop: 16,
+    opacity: 0.6,
+  },
+  imgContainer: {
+    width: screenWidth,
+    height: screenHeight * 0.45,
+    marginVertical: normalizeModerately(25),
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
