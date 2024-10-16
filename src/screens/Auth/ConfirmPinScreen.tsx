3ab35c7847b838a7960b88b9ef906b7d07c5b406
Moazam05 <salmanmoazam08@gmail.com>
2024-10-16 23:41:53 +0500

    ConfirmPinScreen

--- src/screens/Auth/ConfirmPinScreen.tsx
+++ src/screens/Auth/ConfirmPinScreen.tsx
@@ -1,12 +1,97 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {FONTS} from '../../constants/Fonts';
+import {RFValue} from 'react-native-responsive-fontsize';
+import CustomSafeAreaView from '../../components/CustomSafeAreaView';
+import BackButton from '../../components/BackButton';
+import CustomText from '../../components/CustomText';
+import OTPInput from './components/OTPInput';
+import CustomNumberPad from './components/CustomNumberPad';
+import {useNavigation} from '@react-navigation/native';
 
-const ConfirmPinScreen = () => {
+const ConfirmPinScreen = ({route}: any) => {
+  const navigation: any = useNavigation();
+
+  const [otpValues, setOtpValues] = useState(['', '', '', '']);
+  const [focusedIndex, setFocusedIndex] = useState(0);
+  const [otpError, setOtpError] = useState<string | null>(null);
+
+  const handlePressNumber = (number: number | string) => {
+    if (focusedIndex < otpValues.length) {
+      const newOtpValues = [...otpValues];
+      newOtpValues[focusedIndex] = number.toString();
+      setOtpError(null);
+      setOtpValues(newOtpValues);
+      setFocusedIndex(focusedIndex + 1);
+    }
+  };
+
+  const handlePressBackspace = () => {
+    if (focusedIndex > 0) {
+      const newOtpValues = [...otpValues];
+      newOtpValues[focusedIndex - 1] = '';
+      setOtpValues(newOtpValues);
+      setFocusedIndex(focusedIndex - 1);
+    }
+  };
+
+  const handlePressCheckmark = async () => {
+    let valid = false;
+    const isNotEmpty = otpValues.map(i => {
+      if (i == '') {
+        valid = true;
+        setOtpError('Enter all PIN');
+      }
+    });
+
+    if (otpValues.toString() !== route.params.pin) {
+      valid = true;
+      setOtpValues(['', '', '', '']);
+      setFocusedIndex(0);
+      setOtpError('PIN not matching');
+    }
+
+    if (!valid) {
+      navigation.navigate('AccountProtectedScreen');
+    }
+  };
+
   return (
-    <View>
-      <Text>ConfirmPinScreen</Text>
-    </View>
+    <CustomSafeAreaView>
+      <BackButton />
+      <CustomText
+        variant="h5"
+        fontFamily={FONTS.Medium}
+        style={styles.mainContainer}>
+        Confirm your Groww PIN
+      </CustomText>
+      <CustomText style={styles.subText}>
+        Re-enter your Groww PIN for confimation.
+      </CustomText>
+      <OTPInput
+        otpValues={otpValues}
+        error={otpError}
+        focusedIndex={focusedIndex}
+      />
+
+      <CustomNumberPad
+        onPressNumber={handlePressNumber}
+        onPressBackspace={handlePressBackspace}
+        onPressCheckmark={handlePressCheckmark}
+      />
+    </CustomSafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  subText: {
+    opacity: 0.8,
+    fontSize: RFValue(9.5),
+  },
+});
 
 export default ConfirmPinScreen;
