19bb2ab6bb765b4fb03838c48a4ac7f4382c8324
Moazam05 <salmanmoazam08@gmail.com>
2024-10-16 18:10:39 +0500

    CustomInput

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,11 @@
 import LoginImageDark from './login_dark_animation.png';
 import LoginImageLight from './login_animation_light.png';
 import GoogleIcon from './google.png';
+import Logo from './logo_text.png';
 
 export const Images = {
   LoginImageDark,
   LoginImageLight,
   GoogleIcon,
+  Logo,
 };
--- src/screens/Auth/EmailScreen.tsx
+++ src/screens/Auth/EmailScreen.tsx
@@ -1,11 +1,17 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import React from 'react';
+import CustomSafeAreaView from '../../components/CustomSafeAreaView';
+import BackButton from '../../components/BackButton';
+import CenteredLogo from './components/CenteredLogo';
 
 const EmailScreen = () => {
   return (
-    <View>
-      <Text>EmailScreen</Text>
-    </View>
+    <CustomSafeAreaView>
+      <BackButton path="LoginScreen" />
+      <ScrollView>
+        <CenteredLogo />
+      </ScrollView>
+    </CustomSafeAreaView>
   );
 };
 
