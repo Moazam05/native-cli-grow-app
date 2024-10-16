bb46cb90b70a803469696428ec0df885f8b4942a
Moazam05 <salmanmoazam08@gmail.com>
2024-10-16 12:31:39 +0500

    Navigation

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/Home/HomeScreen';
+import LoginScreen from '../screens/Auth/LoginScreen';
+import {useCustomTheme} from '../themes/Theme';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  const theme = useCustomTheme();
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: theme.colors.background,
+      text: theme.colors.text,
+      card: theme.colors.card,
+      border: theme.colors.border,
+      notification: theme.colors.notification,
+      primary: theme.colors.primary,
+    },
+  };
+
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator
+        initialRouteName="BottomTabBar"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
