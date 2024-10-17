5abaa9cbd80bced89935fe9e6bffdf6f1dc20a67
Moazam05 <salmanmoazam08@gmail.com>
2024-10-17 12:14:49 +0500

    ForgotPassword

--- src/screens/Auth/ForgotPassword.tsx
+++ src/screens/Auth/ForgotPassword.tsx
@@ -1,12 +1,197 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {View, Text, Alert, StyleSheet} from 'react-native';
+import {useNavigation, useTheme} from '@react-navigation/native';
+import {RFValue} from 'react-native-responsive-fontsize';
+import CustomSafeAreaView from '../../components/CustomSafeAreaView';
+import BackButton from '../../components/BackButton';
+import CustomInput from '../../components/CustomInput';
+import OtpTimer from './components/OtpTimer';
+import CustomText from '../../components/CustomText';
+import GuidelineText from './components/GuidelineText';
+import CustomButton from '../../components/CustomButton';
 
-const ForgotPassword = () => {
+const validatePasswordLength = (password: string) => {
+  const regex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+  return regex.test(password);
+};
+
+const validatePasswordEntry = (
+  password: string,
+  name: string,
+  email: string,
+) => {
+  if (!validatePasswordLength(password)) {
+    return {msg: 'Password length must be 8 to 20 characters', result: false};
+  }
+  if (name && password.toLowerCase().includes(name.toLowerCase())) {
+    return {msg: "Must not contain user's name", result: false};
+  }
+
+  if (email && password.toLowerCase().includes(email.toLowerCase())) {
+    return {msg: "Must not contain user's email id", result: false};
+  }
+
+  return {msg: 'Passed Local Test!', result: true};
+};
+
+interface PasswordInputs {
+  password: string;
+  confirmpassword: string;
+  otp: string;
+}
+
+const ForgotPassword = ({route}: any) => {
+  const navigation: any = useNavigation();
+  const {colors} = useTheme();
+
+  const [otpSent, setOtpSent] = useState(false);
+  const [inputs, setInputs] = useState<PasswordInputs>({
+    password: '',
+    confirmpassword: '',
+    otp: '',
+  });
+  const [errors, setErrors] = useState<{[key: string]: string | undefined}>();
+  const [loading, setLoading] = useState(false);
+
+  const handleOnChange = (text: string, fieldName: string) => {
+    setInputs(prevInputs => ({
+      ...prevInputs,
+      [fieldName]: text,
+    }));
+    // Clear the error when the user starts typing again
+    setErrors(prevErrors => ({
+      ...prevErrors,
+      [fieldName]: undefined,
+    }));
+  };
+
+  const validateForm = () => {
+    const newErrors: {[key: string]: string | undefined} = {};
+    if (!inputs.password.trim()) {
+      newErrors.password = 'Enter new password';
+    }
+    if (!inputs.confirmpassword.trim()) {
+      newErrors.confirmpassword = 'Enter confirm password';
+    }
+    if (
+      !validatePasswordEntry(inputs.password, 'test', route?.params?.email)
+        .result
+    ) {
+      newErrors.password =
+        'Set a stronger password, kindly refer to guidelines below.';
+    }
+
+    if (inputs?.confirmpassword !== inputs?.password) {
+      newErrors.confirmpassword = 'Confirm Password not match';
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleUpdatePassword = async () => {
+    if (validateForm()) {
+      setLoading(true);
+      //
+      setOtpSent(true);
+      setLoading(false);
+    }
+  };
+
+  const verifyOtp = async () => {
+    setLoading(true);
+    navigation.navigate('LoginScreen');
+    setLoading(false);
+  };
+
   return (
-    <View>
-      <Text>ForgotPassword</Text>
-    </View>
+    <CustomSafeAreaView>
+      <BackButton />
+      <CustomInput
+        label="NEW PASSWORD"
+        placeholder="8-20 Characters"
+        value={inputs?.password}
+        error={errors?.password}
+        onChangeText={text => handleOnChange(text, 'password')}
+        password
+      />
+      <CustomInput
+        label="CONFIRM NEW PASSWORD"
+        placeholder="8-20 Characters"
+        error={errors?.confirmpassword}
+        value={inputs.confirmpassword}
+        onChangeText={text => handleOnChange(text, 'confirmpassword')}
+        password
+      />
+
+      {otpSent && (
+        <CustomInput
+          error={errors?.otp}
+          value={inputs.otp}
+          keyboardType="number-pad"
+          returnKeyType="done"
+          onSubmitEditing={verifyOtp}
+          rightIcon={
+            <OtpTimer
+              style={{
+                color: colors.text,
+                opacity: 0.8,
+                fontSize: RFValue(10),
+                right: 20,
+              }}
+              type="email"
+              onPress={() => handleUpdatePassword()}
+            />
+          }
+          maxLength={6}
+          onChangeText={text => handleOnChange(text, 'otp')}
+        />
+      )}
+
+      <View style={styles.bottomBtn}>
+        {errors?.otp && (
+          <View style={styles.errorContainer}>
+            <CustomText variant="h7">
+              Wrong OTP, 2 attempts remaining
+            </CustomText>
+          </View>
+        )}
+        <GuidelineText
+          text={[
+            'Password must have at least one uppercase and lowercase letter.',
+            'Must contain at least one number and one special character',
+            "Must not contain user's first/last name & email id",
+            'Must be different from previous password',
+          ]}
+        />
+        <CustomButton
+          disabled={loading}
+          loading={loading}
+          text={otpSent ? 'UPDATE PASSWORD' : 'SEND OTP'}
+          onPress={otpSent ? verifyOtp : handleUpdatePassword}
+        />
+      </View>
+    </CustomSafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  errorContainer: {
+    backgroundColor: 'rgba(255, 0, 0, 0.2)',
+    padding: 10,
+    justifyContent: 'center',
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  bottomBtn: {
+    position: 'absolute',
+    bottom: 10,
+    width: '100%',
+    right: 10,
+    left: 10,
+  },
+});
 
 export default ForgotPassword;
