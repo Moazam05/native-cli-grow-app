eed31ebc4f80ed8019cb2b8f011fb3e75f1e730c
Moazam05 <salmanmoazam08@gmail.com>
2024-10-21 14:59:42 +0500

    redux setup

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {GOOGLE_WEB_CLIENT_ID, GOOGLE_IOS_CLIENT_ID} from '@env';
 import Toast from 'react-native-toast-message';
 import {toastConfig} from './src/components/ToastConfig/ToastConfig';
+import {Provider} from 'react-redux';
+import {persistor, store} from './src/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 GoogleSignin.configure({
   webClientId: GOOGLE_WEB_CLIENT_ID,
@@ -14,7 +17,11 @@ GoogleSignin.configure({
 
 const App = () => (
   <>
-    <Navigation />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+    </Provider>
     <Toast
       visibilityTime={2500}
       config={toastConfig}
